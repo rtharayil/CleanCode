3c418986b6ec62ed8987791e7c286ae167fe5e73
rtharayil <ranjiththarayil@gmail.com>
2025-03-25 14:07:23 +0530

    fix

--- Functions/review2.cs
+++
@@ -1,9 +0,0 @@
-
-var name = "Ryan McDermott";
-
-public void PrintAsFirstNameAndLastName()
-{
-    var temp = name.Split(" ");
-    name = $"His first name is {temp[0]}, and his last name is {temp[1]}"; 
-    Console.WriteLine(name);
-}
